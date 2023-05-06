ccce70407028e2f9907238db66482981c979e43e
MelihDemirtel <melihcandemirtel@gmail.com>
2023-05-06 13:44:00 +0300

    added HomePage

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private JavascriptExecutor jsx;
+    private Actions actions;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        jsx = (JavascriptExecutor) driver;
+        this.actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+    public void verifyHomePageIsOpened() {
+        String expectedUrl = "https://demoqa.com";
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+    }
+}
